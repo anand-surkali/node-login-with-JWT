625e189e10f38f1c6952dbf30694be70ed4f3fed
anand-surkali <mailtoanand333@gmail.com>
2021-08-10 18:05:54 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+// const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,10 +9,16 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
   
-  app.get('*', (req, res) => {
-    console.log(__dirname);
-    res.sendFile(path.resolve(__dirname, '../project/build', 'index.html'))
-  });
+  // app.get('*', (req, res) => {
+  //   console.log(__dirname);
+  //   res.sendFile(path.resolve(__dirname, '../project/build/index.html'))
+  // });
+
+const root = require('path').join(__dirname, 'project', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
     
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
